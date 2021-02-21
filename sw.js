c22b19a9555de1c8dc776cc03eef31153a98eb7f
Omega0x013 <xanderbielby@gmail.com>
2021-02-21 18:46:49 +0000

    scaling square canvas

---
+++ sw.js
@@ -0,0 +1,3 @@
+/* Service Worker for Exam Maker */
+self.addEventListener('install', (event) => {});
+self.addEventListener('fetch',function(event){event.respondWith(caches.match(event.request).then(function(response){if(response){return response;}return fetch(event.request).then(function(response){if(!response||response.status!==200||response.type!=='basic'){return response;}var responseToCache = response.clone();caches.open("runtime_cache").then(function(cache){cache.put(event.request, responseToCache);});return response;});}));});
